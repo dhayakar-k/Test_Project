2099d94cf1aeee71cf04fffe6d85ddd1def139c8
Harika <harikasuguru2000@gmail.com>
2024-03-14 23:22:59 +0530

    code review comments resolved

--- src/acl/Harika.java
+++ src/acl/Harika.java
@@ -4,6 +4,11 @@ public class Harika {
     public static void main(String[] args) {
         String actualName = "Dhayakar";
         String ExpectedName = "Harika";
+        if (actualName.equalsIgnoreCase(ExpectedName)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
         System.out.println(actualName.equalsIgnoreCase(ExpectedName));
 
     }
