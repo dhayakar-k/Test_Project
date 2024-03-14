bdac53d627479783cad14adfbb73d582f9377d42
Harika <harikasuguru2000@gmail.com>
2024-03-14 22:56:07 +0530

    first git commit

---
+++ src/acl/Harika.java
@@ -0,0 +1,9 @@
+package acl;
+
+public class Harika {
+    public static void main(String[] args) {
+        String actualName = "Dhayakar";
+        String ExpectedName = "Harika";
+        System.out.println(actualName.equalsIgnoreCase(ExpectedName));
+    }
+}
